b74282f2fc7532466350e8059b22d6e35ebedeb9
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-11-19 22:57:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ function App() {
       },
     ];
     return (
-      (<div className="flex flex-col items-center justify-center h-[100%] m-auto">
+      (<div className="flex flex-col items-center justify-center ">
         <TypewriterEffectSmooth words={words} />
         <motion.div
 
