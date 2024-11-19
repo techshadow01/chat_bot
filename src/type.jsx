f096d4cd0f2f1db1c061bfce228ab31addf23b05
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-11-19 22:48:43 +0530

    Update type.jsx

--- src/type.jsx
+++ src/type.jsx
@@ -103,7 +103,6 @@ export const TypewriterEffectSmooth = ({
                   {char}
                 </span>
               ))}
-              <span className="dark:text-white text-black ml-[2vw]"></span>
             </div>)
           );
         })}
