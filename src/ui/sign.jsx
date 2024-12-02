40dbcfa34239f778871483919ad9e77ff5aabd3b
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-12-02 12:46:13 +0530

    Add files via upload

---
+++ src/ui/sign.jsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import { useSpring, animated } from '@react-spring/web';
+import Csign from './csign'
+import Slide from '@mui/material/Slide';
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+    const {
+        children,
+        in: open,
+        onClick,
+        onEnter,
+        onExited,
+        ownerState,
+        ...other
+    } = props;
+    const style = useSpring({
+        from: { opacity: 0 },
+        to: { opacity: open ? 1 : 0 },
+        onStart: () => {
+            if (open && onEnter) {
+                onEnter(null, true);
+            }
+        },
+        onRest: () => {
+            if (!open && onExited) {
+                onExited(null, true);
+            }
+        },
+    });
+
+    return (
+        <animated.div ref={ref} style={style} {...other}>
+            {React.cloneElement(children, { onClick })}
+        </animated.div>
+    );
+});
+
+Fade.propTypes = {
+    children: PropTypes.element.isRequired,
+    in: PropTypes.bool,
+    onClick: PropTypes.any,
+    onEnter: PropTypes.func,
+    onExited: PropTypes.func,
+    ownerState: PropTypes.any,
+};
+
+const style = {
+    position: 'absolute',
+    margin: 'auto',
+    bgcolor: 'transparent',
+    p: 4,
+};
+
+const sign = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+            <Button style={{ color: '#7F39E8' }} onClick={handleOpen}>Login</Button>
+            <Modal
+                aria-labelledby="spring-modal-title"
+                aria-describedby="spring-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                    },
+                }}
+
+            >
+                <Slide direction="up" className='flexx items-center justify-center m-auto' in={open} mountOnEnter unmountOnExit>
+                    <Box sx={{ height: 180, width: 600, position: 'relative', top: '20%', zIndex: 1 }}>
+                        <Csign />
+                    </Box>
+                </Slide>
+            </Modal>
+        </div>
+    );
+}
+
+export default sign
